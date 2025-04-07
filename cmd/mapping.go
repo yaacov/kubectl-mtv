eb63de3152261b7f741124ed62278197e63f4053
yaacov <kobi.zamir@gmail.com>
2025-04-07 13:33:34 +0300

    more-flags
    Signed-off-by: yaacov <kobi.zamir@gmail.com>

--- cmd/mapping.go
+++ cmd/mapping.go
@@ -1,8 +1,6 @@
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/spf13/cobra"
 	"github.com/yaacov/kubectl-mtv/pkg/client"
 	"github.com/yaacov/kubectl-mtv/pkg/mapping"
@@ -23,53 +21,51 @@ func newMappingCmd() *cobra.Command {
 }
 
 func newCreateNetworkMappingCmd() *cobra.Command {
-	var name, sourceProvider, targetProvider string
+	var sourceProvider, targetProvider string
 	var fromFile string
 
 	cmd := &cobra.Command{
-		Use:   "create-network",
+		Use:   "create-network NAME",
 		Short: "Create a network map",
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			// Get name from positional argument
+			name := args[0]
+
 			// Resolve the appropriate namespace based on context and flags
 			namespace := client.ResolveNamespace(kubeConfigFlags)
 			return mapping.CreateNetwork(kubeConfigFlags, name, namespace, sourceProvider, targetProvider, fromFile)
 		},
 	}
 
-	cmd.Flags().StringVar(&name, "name", "", "Mapping name")
 	cmd.Flags().StringVar(&sourceProvider, "source", "", "Source provider name")
 	cmd.Flags().StringVar(&targetProvider, "target", "", "Target provider name")
 	cmd.Flags().StringVar(&fromFile, "from-file", "", "Create from YAML file")
-
-	if err := cmd.MarkFlagRequired("name"); err != nil {
-		fmt.Printf("Warning: error marking 'provider' flag as required: %v\n", err)
-	}
 
 	return cmd
 }
 
 func newCreateStorageMappingCmd() *cobra.Command {
-	var name, sourceProvider, targetProvider string
+	var sourceProvider, targetProvider string
 	var fromFile string
 
 	cmd := &cobra.Command{
-		Use:   "create-storage",
+		Use:   "create-storage NAME",
 		Short: "Create a storage map",
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			// Get name from positional argument
+			name := args[0]
+
 			// Resolve the appropriate namespace based on context and flags
 			namespace := client.ResolveNamespace(kubeConfigFlags)
 			return mapping.CreateStorage(kubeConfigFlags, name, namespace, sourceProvider, targetProvider, fromFile)
 		},
 	}
 
-	cmd.Flags().StringVar(&name, "name", "", "Mapping name")
 	cmd.Flags().StringVar(&sourceProvider, "source", "", "Source provider name")
 	cmd.Flags().StringVar(&targetProvider, "target", "", "Target provider name")
 	cmd.Flags().StringVar(&fromFile, "from-file", "", "Create from YAML file")
-
-	if err := cmd.MarkFlagRequired("name"); err != nil {
-		fmt.Printf("Warning: error marking 'provider' flag as required: %v\n", err)
-	}
 
 	return cmd
 }
