eab008a8bea46ff4db8dc99b566e01fe72b47a7e
yaacov <kobi.zamir@gmail.com>
2025-07-26 10:27:03 +0300

    detail
    Signed-off-by: yaacov <kobi.zamir@gmail.com>

--- pkg/inventory/client.go
+++ pkg/inventory/client.go
@@ -83,8 +83,8 @@ func (pc *ProviderClient) GetResourceCollection(collection string, detail int) (
 }
 
 // GetResourceByID fetches a specific resource by ID
-func (pc *ProviderClient) GetResourceByID(collection, id string) (interface{}, error) {
-	return pc.GetResource(fmt.Sprintf("%s/%s", collection, id))
+func (pc *ProviderClient) GetResourceByID(collection, id string, detail int) (interface{}, error) {
+	return pc.GetResourceWithQuery(fmt.Sprintf("%s/%s", collection, id), fmt.Sprintf("detail=%d", detail))
 }
 
 // oVirt Provider Resources
@@ -92,80 +92,80 @@ func (pc *ProviderClient) GetDataCenters(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("datacenters", detail)
 }
 
-func (pc *ProviderClient) GetDataCenter(id string) (interface{}, error) {
-	return pc.GetResourceByID("datacenters", id)
+func (pc *ProviderClient) GetDataCenter(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("datacenters", id, detail)
 }
 
 func (pc *ProviderClient) GetClusters(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("clusters", detail)
 }
 
-func (pc *ProviderClient) GetCluster(id string) (interface{}, error) {
-	return pc.GetResourceByID("clusters", id)
+func (pc *ProviderClient) GetCluster(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("clusters", id, detail)
 }
 
 func (pc *ProviderClient) GetHosts(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("hosts", detail)
 }
 
-func (pc *ProviderClient) GetHost(id string) (interface{}, error) {
-	return pc.GetResourceByID("hosts", id)
+func (pc *ProviderClient) GetHost(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("hosts", id, detail)
 }
 
 func (pc *ProviderClient) GetVMs(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("vms", detail)
 }
 
-func (pc *ProviderClient) GetVM(id string) (interface{}, error) {
-	return pc.GetResourceByID("vms", id)
+func (pc *ProviderClient) GetVM(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("vms", id, detail)
 }
 
 func (pc *ProviderClient) GetStorageDomains(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("storagedomains", detail)
 }
 
-func (pc *ProviderClient) GetStorageDomain(id string) (interface{}, error) {
-	return pc.GetResourceByID("storagedomains", id)
+func (pc *ProviderClient) GetStorageDomain(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("storagedomains", id, detail)
 }
 
 func (pc *ProviderClient) GetNetworks(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("networks", detail)
 }
 
-func (pc *ProviderClient) GetNetwork(id string) (interface{}, error) {
-	return pc.GetResourceByID("networks", id)
+func (pc *ProviderClient) GetNetwork(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("networks", id, detail)
 }
 
 func (pc *ProviderClient) GetDisks(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("disks", detail)
 }
 
-func (pc *ProviderClient) GetDisk(id string) (interface{}, error) {
-	return pc.GetResourceByID("disks", id)
+func (pc *ProviderClient) GetDisk(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("disks", id, detail)
 }
 
 func (pc *ProviderClient) GetDiskProfiles(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("diskprofiles", detail)
 }
 
-func (pc *ProviderClient) GetDiskProfile(id string) (interface{}, error) {
-	return pc.GetResourceByID("diskprofiles", id)
+func (pc *ProviderClient) GetDiskProfile(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("diskprofiles", id, detail)
 }
 
 func (pc *ProviderClient) GetNICProfiles(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("nicprofiles", detail)
 }
 
-func (pc *ProviderClient) GetNICProfile(id string) (interface{}, error) {
-	return pc.GetResourceByID("nicprofiles", id)
+func (pc *ProviderClient) GetNICProfile(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("nicprofiles", id, detail)
 }
 
 func (pc *ProviderClient) GetWorkloads(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("workloads", detail)
 }
 
-func (pc *ProviderClient) GetWorkload(id string) (interface{}, error) {
-	return pc.GetResourceByID("workloads", id)
+func (pc *ProviderClient) GetWorkload(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("workloads", id, detail)
 }
 
 func (pc *ProviderClient) GetTree() (interface{}, error) {
@@ -182,24 +182,24 @@ func (pc *ProviderClient) GetDatastores(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("datastores", detail)
 }
 
-func (pc *ProviderClient) GetDatastore(id string) (interface{}, error) {
-	return pc.GetResourceByID("datastores", id)
+func (pc *ProviderClient) GetDatastore(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("datastores", id, detail)
 }
 
 func (pc *ProviderClient) GetResourcePools(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("resourcepools", detail)
 }
 
-func (pc *ProviderClient) GetResourcePool(id string) (interface{}, error) {
-	return pc.GetResourceByID("resourcepools", id)
+func (pc *ProviderClient) GetResourcePool(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("resourcepools", id, detail)
 }
 
 func (pc *ProviderClient) GetFolders(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("folders", detail)
 }
 
-func (pc *ProviderClient) GetFolder(id string) (interface{}, error) {
-	return pc.GetResourceByID("folders", id)
+func (pc *ProviderClient) GetFolder(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("folders", id, detail)
 }
 
 // OpenStack Provider Resources
@@ -208,113 +208,113 @@ func (pc *ProviderClient) GetInstances(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("instances", detail)
 }
 
-func (pc *ProviderClient) GetInstance(id string) (interface{}, error) {
-	return pc.GetResourceByID("instances", id)
+func (pc *ProviderClient) GetInstance(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("instances", id, detail)
 }
 
 func (pc *ProviderClient) GetImages(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("images", detail)
 }
 
-func (pc *ProviderClient) GetImage(id string) (interface{}, error) {
-	return pc.GetResourceByID("images", id)
+func (pc *ProviderClient) GetImage(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("images", id, detail)
 }
 
 func (pc *ProviderClient) GetFlavors(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("flavors", detail)
 }
 
-func (pc *ProviderClient) GetFlavor(id string) (interface{}, error) {
-	return pc.GetResourceByID("flavors", id)
+func (pc *ProviderClient) GetFlavor(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("flavors", id, detail)
 }
 
 func (pc *ProviderClient) GetSubnets(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("subnets", detail)
 }
 
-func (pc *ProviderClient) GetSubnet(id string) (interface{}, error) {
-	return pc.GetResourceByID("subnets", id)
+func (pc *ProviderClient) GetSubnet(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("subnets", id, detail)
 }
 
 func (pc *ProviderClient) GetPorts(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("ports", detail)
 }
 
-func (pc *ProviderClient) GetPort(id string) (interface{}, error) {
-	return pc.GetResourceByID("ports", id)
+func (pc *ProviderClient) GetPort(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("ports", id, detail)
 }
 
 func (pc *ProviderClient) GetVolumeTypes(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("volumetypes", detail)
 }
 
-func (pc *ProviderClient) GetVolumeType(id string) (interface{}, error) {
-	return pc.GetResourceByID("volumetypes", id)
+func (pc *ProviderClient) GetVolumeType(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("volumetypes", id, detail)
 }
 
 func (pc *ProviderClient) GetVolumes(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("volumes", detail)
 }
 
-func (pc *ProviderClient) GetVolume(id string) (interface{}, error) {
-	return pc.GetResourceByID("volumes", id)
+func (pc *ProviderClient) GetVolume(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("volumes", id, detail)
 }
 
 func (pc *ProviderClient) GetSecurityGroups(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("securitygroups", detail)
 }
 
-func (pc *ProviderClient) GetSecurityGroup(id string) (interface{}, error) {
-	return pc.GetResourceByID("securitygroups", id)
+func (pc *ProviderClient) GetSecurityGroup(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("securitygroups", id, detail)
 }
 
 func (pc *ProviderClient) GetFloatingIPs(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("floatingips", detail)
 }
 
-func (pc *ProviderClient) GetFloatingIP(id string) (interface{}, error) {
-	return pc.GetResourceByID("floatingips", id)
+func (pc *ProviderClient) GetFloatingIP(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("floatingips", id, detail)
 }
 
 func (pc *ProviderClient) GetProjects(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("projects", detail)
 }
 
-func (pc *ProviderClient) GetProject(id string) (interface{}, error) {
-	return pc.GetResourceByID("projects", id)
+func (pc *ProviderClient) GetProject(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("projects", id, detail)
 }
 
-// OpenShift Provider Resources
+// Kubernetes/OpenShift Provider Resources
 func (pc *ProviderClient) GetStorageClasses(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("storageclasses", detail)
 }
 
-func (pc *ProviderClient) GetStorageClass(id string) (interface{}, error) {
-	return pc.GetResourceByID("storageclasses", id)
+func (pc *ProviderClient) GetStorageClass(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("storageclasses", id, detail)
 }
 
 func (pc *ProviderClient) GetPersistentVolumeClaims(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("persistentvolumeclaims", detail)
 }
 
-func (pc *ProviderClient) GetPersistentVolumeClaim(id string) (interface{}, error) {
-	return pc.GetResourceByID("persistentvolumeclaims", id)
+func (pc *ProviderClient) GetPersistentVolumeClaim(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("persistentvolumeclaims", id, detail)
 }
 
 func (pc *ProviderClient) GetNamespaces(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("namespaces", detail)
 }
 
-func (pc *ProviderClient) GetNamespace(id string) (interface{}, error) {
-	return pc.GetResourceByID("namespaces", id)
+func (pc *ProviderClient) GetNamespace(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("namespaces", id, detail)
 }
 
 func (pc *ProviderClient) GetDataVolumes(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("datavolumes", detail)
 }
 
-func (pc *ProviderClient) GetDataVolume(id string) (interface{}, error) {
-	return pc.GetResourceByID("datavolumes", id)
+func (pc *ProviderClient) GetDataVolume(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("datavolumes", id, detail)
 }
 
 // OVA Provider Resources
@@ -322,8 +322,8 @@ func (pc *ProviderClient) GetOVAFiles(detail int) (interface{}, error) {
 	return pc.GetResourceCollection("ovafiles", detail)
 }
 
-func (pc *ProviderClient) GetOVAFile(id string) (interface{}, error) {
-	return pc.GetResourceByID("ovafiles", id)
+func (pc *ProviderClient) GetOVAFile(id string, detail int) (interface{}, error) {
+	return pc.GetResourceByID("ovafiles", id, detail)
 }
 
 // Generic helper functions for provider-agnostic operations
